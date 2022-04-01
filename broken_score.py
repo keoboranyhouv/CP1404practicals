a15d582f92c63c8dfaff34842ffa8a688316854d
keoboranyhouv <h.keoborany@gmail.com>
2022-04-01 16:35:20 +0800

    Add broken_score program

--- broken_score.py
+++ broken_score.py
@@ -6,14 +6,20 @@ Broken program to determine score status
 # TODO: Fix this!
 
 score = float(input("Enter score: "))
-if 0 <= score <= 100:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
+
+
+def generate_results():
+    if 0 <= score <= 100:
+        if score >= 90:
+            print("Excellent")
+        elif score >= 50:
+            print("Passable")
+        else:
+            print("Bad")
     else:
-        print("Bad")
-else:
-    print("Invalid score!")
+        print("Invalid score!")
+
+
+generate_results()
 
     
