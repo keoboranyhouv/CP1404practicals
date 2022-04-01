2a7ed4e74eeb727ab45c6153237d273ffcc302cb
keoboranyhouv <h.keoborany@gmail.com>
2022-04-01 16:30:15 +0800

    Refactor temperature convertor program into functions

--- temperatures.py
+++ temperatures.py
@@ -1,22 +1,34 @@
-"""
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-"""
-
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
+
+
+def main():
+    print(MENU)
+
+
 choice = input(">>> ").upper()
+
+
+def convert_celsius_to_fahrenheit():
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def convert_fahrenheit_to_celsius():
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = convert_celsius_to_fahrenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         # TODO: Write this section to convert F to C and display the result
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = convert_fahrenheit_to_celsius()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
