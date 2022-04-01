04e1faa59fd803b8b34286be9682c9f2a0612c21
keoboranyhouv <h.keoborany@gmail.com>
2022-04-01 16:47:53 +0800

    Add random score and print results generator

--- broken_score.py
+++ broken_score.py
@@ -1,18 +1,27 @@
+import random
+
+
 def main():
     score = float(input("Enter score: "))
-    generate_results(score)
+    result = generate_results(score)
+    print(f"Result: {result}")
+
+    random_score = random.randint(0,100)
+    print("Random score:",random_score)
+    random_result = generate_results(random_score)
+    print(f"Result: {random_result}")
 
 
 def generate_results(score):
     if 0 <= score <= 100:
         if score >= 90:
-            print("Excellent")
+            return "Excellent"
         elif score >= 50:
-            print("Passable")
+            return "Passable"
         else:
-            print("Bad")
+            return "Bad"
     else:
-        print("Invalid score!")
+        return "Invalid score!"
 
 
 if __name__ == '__main__':
